0cec450431bb92d7e75f138083510baa27bf2f44
Michal Pawlik <admin@michalp.net>
2024-08-13 17:50:33 +0200

    Reorganize the landing page layout, update some links

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -2,77 +2,32 @@ import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
-type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: JSX.Element;
-};
-
-type FeatureItemX = {
-  title: string;
-  emojiIcon: string;
-  description: JSX.Element;
-};
-
-const FeatureList: FeatureItemX[] = [
-  {
-    title: 'Batteries Included',
-    emojiIcon: '💡',
-    description: (
-      <>
-	It is as simple as adding an sbt plugin.
-      </>
-    ),
-  },
-  {
-    title: 'Powerful',
-    emojiIcon: '🚀',
-    description: (
-      <>
-	Automate your test updates for any ADT.
-      </>
-    ),
-  },
-  {
-    title: 'Flexible',
-    emojiIcon: '🔌',
-    description: (
-      <>
-	Supports inline and file snapshots.
-      </>
-    ),
-  }
-];
-
-function Feature({title, emojiIcon, description}: FeatureItemX) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <p className={styles.emojiIcon}>{emojiIcon}</p>
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
-
 export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-        <div className="row">
-          <div className="text--center">
-            <Heading as="h2">See it in action 📸</Heading>
-            <video width="100%" height="auto" controls>
-              <source src="https://github.com/user-attachments/assets/8e2f3037-ecb0-4f6b-a9ba-fd277d2af55d"/>
-            </video>
+          <div className={clsx('col col--9')}>
+            <div className="text--center">
+              {/* <Heading as="h2">See it in action</Heading> */}
+              <video width="100%" height="auto" controls>
+                <source src="https://github.com/user-attachments/assets/8e2f3037-ecb0-4f6b-a9ba-fd277d2af55d"/>
+              </video>
+            </div>
+          </div>
+          <div className={clsx('col col--3')}>
+            <div className="text">
+              <Heading as="h2">See it in action</Heading>
+              <p>
+              {/* TODO: write something meaningful before merge */}
+                Aut suscipit doloremque omnis pariatur. Similique ut incidunt totam. Earum distinctio nihil quasi eum occaecati deserunt.
+
+Eius rerum vitae eos repellat impedit ea. Nesciunt itaque qui et. Maxime consequatur rerum hic tempore necessitatibus quam id. Tempore dolore enim non ea autem.
+
+
+
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -12,17 +12,30 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/intro">
-            Get started 🚀
-          </Link>
+
+        <div className="row">
+          <div className={clsx('col col--6')}>
+
+            <Heading as="h1" className="hero__title">
+              {siteConfig.title}
+            </Heading>
+            <p className="hero__subtitle">{siteConfig.tagline}</p>
+
+          </div>
+
+          <div className={clsx('col col--6', styles.justifyButtonVertically)}>
+            <div className={styles.buttons}>
+              <Link
+                className="button button--secondary button--lg"
+                to="/quick-start">
+                Get started 📸
+              </Link>
+            </div>
+          </div>
+
+
         </div>
+
       </div>
     </header>
   );
