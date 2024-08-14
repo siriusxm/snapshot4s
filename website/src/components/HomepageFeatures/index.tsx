aac9723a800fc9cf810a3af60e1e1f71918adc02
Michal Pawlik <admin@michalp.net>
2024-08-14 08:35:02 +0200

    clean up before merge

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -9,7 +9,6 @@ export default function HomepageFeatures(): JSX.Element {
         <div className="row">
           <div className={clsx('col col--9')}>
             <div className="text--center">
-              {/* <Heading as="h2">See it in action</Heading> */}
               <video width="100%" height="auto" controls>
                 <source src="https://github.com/user-attachments/assets/8e2f3037-ecb0-4f6b-a9ba-fd277d2af55d"/>
               </video>
@@ -18,11 +17,10 @@ export default function HomepageFeatures(): JSX.Element {
           <div className={clsx('col col--3')}>
             <div className="text">
               <Heading as="h2">See it in action</Heading>
-              <p>
-	      Snapshot4s automates the process of writing and updating tests, reducing the maintenance burden for developers.
-              </p>
-	      <p>See what happens to <code>???</code> and the outdated test value on <code>snapshot4sPromote</code>. </p>
-	      <p><a href="./intro">Learn more.</a></p>
+              <p>Snapshot4s automates the process of writing and updating tests, reducing the maintenance burden for developers.</p>
+              <p>See what happens to <code>???</code> and the outdated test value on <code>snapshot4sPromote</code>. </p>
+              <Heading as="h2">Learn more</Heading>
+              <p>Want to learn more about snapshot testing? Check out our <a href="./intro">intro to learn more</a>.</p>
             </div>
           </div>
         </div>
