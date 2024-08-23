aaf211b02cc673ced50660590567816636547caa
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-08-23 08:30:52 +0200

    Update munit to 1.0.1 (#33)

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   val oslib                  = "0.10.1"
   val weaver                 = "0.8.4"
   val pprint                 = "0.9.0"
-  val munit                  = "1.0.0"
+  val munit                  = "1.0.1"
   val scalatest              = "3.2.19"
   val scalacCompatAnnotation = "0.1.4"
 }
