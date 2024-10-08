46912f7b63ba702adc2dee04ecdaac381f890768
Michal Pawlik <admin@michalp.net>
2024-10-08 10:57:02 +0200

    Update os-lib to 0.11.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,5 +1,5 @@
 object Versions {
-  val oslib                  = "0.10.6"
+  val oslib                  = "0.11.1"
   val weaver                 = "0.8.4"
   val pprint                 = "0.9.0"
   val munit                  = "1.0.2"
