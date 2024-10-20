395bc39ee251919455d2186253f71f13b1f6614d
Scala Steward <scala_steward@virtuslab.com>
2024-10-20 22:12:39 +0000

    Update os-lib to 0.11.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,5 +1,5 @@
 object Versions {
-  val oslib                  = "0.11.1"
+  val oslib                  = "0.11.2"
   val weaver                 = "0.8.4"
   val pprint                 = "0.9.0"
   val munit                  = "1.0.2"
