3a3afabb212e59d31e13420b902d7bcb501d8a32
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-10-24 11:31:48 +0200

    Update os-lib to 0.11.3 (#57)

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,5 +1,5 @@
 object Versions {
-  val oslib                  = "0.11.2"
+  val oslib                  = "0.11.3"
   val weaver                 = "0.8.4"
   val pprint                 = "0.9.0"
   val munit                  = "1.0.2"
