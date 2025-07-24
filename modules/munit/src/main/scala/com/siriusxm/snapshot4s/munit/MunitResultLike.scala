1f3d9b1d1d89fa6f19e3089fd3e6aec6547f7abc
zainab-ali <zainab@kebab-ca.se>
2025-07-24 15:57:57 +0100

    Specify DiffOptions.

--- modules/munit/src/main/scala/com/siriusxm/snapshot4s/munit/MunitResultLike.scala
+++ modules/munit/src/main/scala/com/siriusxm/snapshot4s/munit/MunitResultLike.scala
@@ -16,33 +16,42 @@
 
 package snapshot4s.munit
 
+import _root_.munit.diff.DiffOptions
 import _root_.munit.{Assertions, Location}
 
 import snapshot4s.*
 
 private object MunitResultLike {
 
-  def resultLike[A](loc: Location): ResultLike[A, Unit] = new ResultLike[A, Unit] {
+  def resultLike[A](loc: Location, diffOptions: DiffOptions): ResultLike[A, Unit] =
+    new ResultLike[A, Unit] {
 
-    def apply(result: () => Result[A]): Unit = {
-      resultToAssertion(result(), loc)
+      def apply(result: () => Result[A]): Unit = {
+        resultToAssertion(result(), loc, diffOptions)
+      }
     }
-  }
 
-  private def resultToAssertion[A](result: Result[A], loc: Location): Unit = {
+  private def resultToAssertion[A](
+      result: Result[A],
+      loc: Location,
+      diffOptions: DiffOptions
+  ): Unit = {
     result match {
-      case _: Result.Success[?]            => ()
-      case _: Result.NonExistent[?]        => throw Assertions.fail(ErrorMessages.nonExistent)(loc)
+      case _: Result.Success[?]     => ()
+      case _: Result.NonExistent[?] =>
+        throw Assertions.fail(ErrorMessages.nonExistent)(loc, diffOptions)
       case Result.Failure(found, snapshot) =>
-        throw Assertions.fail(diffReport(found.toString, snapshot.toString))(loc)
+        throw Assertions
+          .fail(diffReport(found.toString, snapshot.toString, diffOptions))(loc, diffOptions)
     }
   }
 
-  private def diffReport(found: String, expected: String): String = {
+  private def diffReport(found: String, expected: String, diffOptions: DiffOptions): String = {
     import _root_.munit.diff.Diff
     val diff = new Diff(
       obtained = found,
-      expected = expected
+      expected = expected,
+      options = diffOptions
     )
     diff.createReport(ErrorMessages.failure, printObtainedAsStripMargin = false)
   }
--- modules/munit/src/main/scala/com/siriusxm/snapshot4s/munit/SnapshotAssertions.scala
+++ modules/munit/src/main/scala/com/siriusxm/snapshot4s/munit/SnapshotAssertions.scala
@@ -17,6 +17,7 @@
 package snapshot4s.munit
 
 import _root_.munit.Location
+import _root_.munit.diff.DiffOptions
 
 import snapshot4s.{ResultLike, SnapshotAssertions as CoreSnapshotAssertions, SnapshotEq}
 
@@ -24,8 +25,11 @@ trait SnapshotAssertions extends CoreSnapshotAssertions[Unit] {
 
   implicit def snapshotEq[A]: SnapshotEq[A] = SnapshotEq.fromUniversalEquals
 
-  implicit def munitResultLike[A](implicit loc: Location): ResultLike[A, Unit] =
-    MunitResultLike.resultLike[A](loc)
+  implicit def munitResultLike[A](implicit
+      loc: Location,
+      diffOptions: DiffOptions
+  ): ResultLike[A, Unit] =
+    MunitResultLike.resultLike[A](loc, diffOptions)
 }
 
 object SnapshotAssertions extends SnapshotAssertions
