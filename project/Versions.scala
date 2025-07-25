03b3ab6a4d74c7b635a35ea52e58be38f5e2636d
Scala Steward <scala_steward@virtuslab.com>
2025-07-25 19:07:17 +0000

    Update pprint to 0.9.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val oslib                  = "0.11.4"
   val weaver                 = "0.9.2"
-  val pprint                 = "0.9.0"
+  val pprint                 = "0.9.1"
   val munit                  = "1.1.1"
   val scalatest              = "3.2.19"
   val scalacCompatAnnotation = "0.1.4"
