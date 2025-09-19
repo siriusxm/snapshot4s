54e3503ed476273c8b6852c69bb89fbbc2e1e8d2
Scala Steward <scala_steward@virtuslab.com>
2025-09-19 20:22:52 +0000

    Update munit to 1.2.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   val oslib                  = "0.11.4"
   val weaver                 = "0.9.2"
   val pprint                 = "0.9.0"
-  val munit                  = "1.1.1"
+  val munit                  = "1.2.0"
   val scalatest              = "3.2.19"
   val scalacCompatAnnotation = "0.1.4"
 }
