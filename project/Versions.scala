daa861bc9640d69ad8bbdfb28bc2aaceb798a45d
Scala Steward <scala_steward@virtuslab.com>
2025-12-09 07:34:35 +0000

    Update weaver-cats, weaver-scalacheck to 0.11.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val oslib                  = "0.11.6"
-  val weaver                 = "0.11.2"
+  val weaver                 = "0.11.3"
   val pprint                 = "0.9.5"
   val munit                  = "1.2.1"
   val scalatest              = "3.2.19"
