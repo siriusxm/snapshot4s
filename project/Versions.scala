e6f555089b9fab7f50afe34e41216eaa69dd62af
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-12-30 15:08:34 +0100

    Update pprint to 0.9.6 (#156)
    Co-authored-by: Michał Pawlik <majk-p@users.noreply.github.com>

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val oslib                  = "0.11.6"
   val weaver                 = "0.11.3"
-  val pprint                 = "0.9.5"
+  val pprint                 = "0.9.6"
   val munit                  = "1.2.1"
   val scalatest              = "3.2.19"
   val scalacCompatAnnotation = "0.1.4"
