cca0c687316e0d346c5fcaeb11d97b64365e89fe
zainab-ali <zainab@kebab-ca.se>
2026-03-18 16:44:58 +0000

    Use synchronized instead of retry.

--- modules/core/src/main/scalanative/com/siriusxm/snapshot4s/PathWrite.scala
+++ modules/core/src/main/scalanative/com/siriusxm/snapshot4s/PathWrite.scala
@@ -22,28 +22,15 @@ import java.nio.file.DirectoryNotEmptyException
 private[snapshot4s] trait PathWrite { path: Path =>
 
   private[snapshot4s] def write(contents: String): Unit = {
-    def retry(count: Int): Unit = try {
-      os.write.over(path.osPath, contents, createFolders = true)
-    } catch {
-      // This retry is necessary for concurrent `write` calls with paths that share the same parent directory.
-      // If the parent doesn't exist, one call creates it and the others raise an exception.
-      // Retrying the call succeeds, as the directory exists.
-      //
-      // Multiple retries are needed if one parent is nested within another.
-      //
-      // === Example ===
-      // The three calls A, B, and C are made concurrently:
-      //  - Call A: snapshot/inline-patch/123-456
-      //  - Call B: snapshot/resource-patch/existing-file.txt
-      //  - Call C: snapshot/resource-patch/nested-dir/nested-file.txt
-      //
-      //  1. A succeeds and creates `snapshot/inline-patch`. B and C fail.
-      //  2. B and C are retried. B creates `snapshot/resource-patch`. C still fails.
-      //  3. C is retried. C creates `snapshot/resource-patch/nested-dir`.
-      case _: DirectoryNotEmptyException | _: IOException if count < 10 =>
-        retry(count + 1)
-    }
-    retry(0)
+    PathWrite.write(path, contents)
   }
+}
 
+private object PathWrite {
+
+  // This synchronized block is necessary for concurrent `write` calls with paths that share the same parent directory.
+  // If the parent doesn't exist, one call creates it and the others raise an exception.
+  def write(path: Path, contents: String): Unit = this.synchronized {
+    os.write.over(path.osPath, contents, createFolders = true)
+  }
 }
